587959f4c292a4fcd866cd6e2b00915b7597938b
dextersiow <68236905+dextersiow@users.noreply.github.com>
2022-01-24 12:00:24 +0000

    Add files via upload

---
+++ lab10/index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>Hashing</title>
+</head>
+<body>
+<?php
+$message = 'd131dd02c5e6eec4693d9a0698aff95c2fcab58712467eab4004583eb8fb7f8955ad340609f4b30283e488832571415a085125e8f7cdc99fd91dbdf280373c5bd8823e3156348f5bae6dacd436c919c6dd53e2b487da03fd02396306d248cda0e99f33420f577ee8ce54b67080a80d1ec69821bcb6a8839396f9652b6ff72a70';
+$message2 = 'd131dd02c5e6eec4693d9a0698aff95c2fcab50712467eab4004583eb8fb7f8955ad340609f4b30283e4888325f1415a085125e8f7cdc99fd91dbd7280373c5bd8823e3156348f5bae6dacd436c919c6dd53e23487da03fd02396306d248cda0e99f33420f577ee8ce54b67080280d1ec69821bcb6a8839396f965ab6ff72a70';
+$hash1 = md5(hex2bin($message), true);
+$hash2 = md5(hex2bin($message2), true);
+
+echo '<p><b>Message1:</b> ' . $message . '</p>';
+echo '<p><b>Message2:</b> ' . $message2 . '</p>';
+echo '<p><b>MD5 1:</b> ' . bin2hex($hash1) . '</p>';
+echo '<p><b>MD5 2:</b> ' . bin2hex($hash2) . '</p>';
+?>
+</body>
+</html>
